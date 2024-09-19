2bf8668b19c6475fabdda2a0f652e07d8f948cce
yimang <lucasbbtung@gmail.com>
2024-09-19 22:09:08 +0800

    Update 20240920-bot.py

--- python進階課/20240920-bot.py
+++ python進階課/20240920-bot.py
@@ -18,29 +18,47 @@ model = genai.GenerativeModel(
   generation_config=generation_config,
 )
 
+message_history = []
 
 # 定義一個函式來方便呼叫api
 def call_api(msg):
     chat_session = model.start_chat(
     history=[
 ])
-    print(":" + msg)
+    print(msg)
     response = chat_session.send_message(msg)
     print(response.text)
     return response.text
+
+# 上傳對話紀錄
+def update_history(msg):
+    message_history.append(msg)
+    if len(message_history) > 200:
+        message_history.pop(0)
+    return "\n".join(message_history)
+
 
 
 @bot.event
 async def on_ready():
     print(f'bot on ready！')
 
+
 # on_message事件
 @bot.event
 async def on_message(msg):
     if msg.author == bot.user:
         return
-    response = call_api(msg.content)
+    global message_history
+    if msg.content.lower() == "reset":
+        message_history = []
+        await msg.channel.send("對話紀錄已清除")
+        return
+    history = update_history("使用者說: " + msg.content)
+    response = call_api(history)
+    update_history(response)
     await msg.channel.send(response)
+
 
 #在colab上執行
 # ================
